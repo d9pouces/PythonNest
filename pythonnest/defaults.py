9623852f95acf25c1424a965c524fca34df69313
d9pouces <github@19pouces.net>
2018-02-04 15:09:11 +0100

    better defaults to handle Heroku hosting

--- pythonnest/defaults.py
+++ pythonnest/defaults.py
@@ -1,3 +1,5 @@
+from djangofloor.conf.callables import DefaultListenAddress
+
 __author__ = 'Matthieu Gallet'
 
 
@@ -13,5 +15,5 @@ READ_ONLY_MIRROR = True
 USE_CELERY = False
 
 FLOOR_INDEX = 'pythonnest.views.index'
-LISTEN_ADDRESS = 'localhost:8130'
+LISTEN_ADDRESS = DefaultListenAddress(8130)
 FLOOR_TEMPLATE_CONTEXT_PROCESSORS = ['pythonnest.context_processors.context_user', ]
